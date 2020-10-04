da331b8ca3f9e86c0cffc0da08c4288b85f62377
sergyi.gonchar <sergyi.gonchar@gmail.com>
2020-10-05 00:16:38 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        return Number ((''+n).split('').splice(1).reverse().join(''));
+    } else {
+        return Number ((''+n).split('').reverse().join(''));
+    }
 }
